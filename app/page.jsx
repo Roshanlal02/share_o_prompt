cb7b4033b0ad9e8256724e74764321ba03599602
Roshan Lal D <roshanlald02@gmail.com>
2023-07-27 12:25:58 +0530

    typo error fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,19 @@
-import Feeds from "@/components/Feeds"
-
+import Feeds from "@/components/Feeds";
 
 const Home = () => {
-    return (
-        <section className="w-full flex-center flex-col">
-            <h1 className="head_text text-center">Discover & Share <br className="max-md:hidden" />
-                <span className="orange_gradient">AI-Powered Prompts</span></h1>
-            <p className="desc text-center">Share 'O' Prompt is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Share 'O' Prompt is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+      <Feeds />
+    </section>
+  );
+};
 
-            <Feeds />
-        </section>
-    )
-}
-
-export default Home
+export default Home;
